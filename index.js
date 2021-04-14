1863d2c4b2adee1f0cc028565193c87469a697e6
tchartchke <chartai.kl@gmail.com>
2021-04-14 14:44:15 -0500

    done

--- index.js
+++ index.js
@@ -12,5 +12,8 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(title => {
+    const words = title.split(" ")
+    return words.map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(" ")
+  });
 }
